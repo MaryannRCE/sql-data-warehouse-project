9687453a489f169a51a9d715000f102e52168517
MaryannRCE <maryannmarasigan1994@gmail.com>
2026-01-09 09:17:05 +0800

    Update scriptsinit_database.sql

--- scriptsinit_database.sql
+++ scriptsinit_database.sql
@@ -4,11 +4,12 @@ Create Database and Schemas
 ===============================
 
 Script Purpose:
-  This script creates a new database named 'DataWarehouse_Baraa' after checking if it already exixts. If the database exists, it is dropped and recreated.
+  This script creates a new database named 'DataWarehouse' after checking if it already exixts.
+	If the database exists, it is dropped and recreated.
   Additionally, the script sets up three schemas within the database: 'bronze', 'silver', 'gold'.
 
 WARNING:
-  Running this script will drop the entire 'DataWarehouse_Baraa' database if it exists.
+  Running this script will drop the entire 'DataWarehouse' database if it exists.
   All data in the database will be permanently deleted. Proceed with caution and ensure
   you have proper backups before running this script.
 */
@@ -18,18 +19,18 @@ USE master;
 GO
 
 -- Best practice: Drop and recreate the database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse_Baraa')
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
-  ALTER DATABASE DataWarehouse_Baraa SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-  DROP DATABASE DataWarehouse_Baraa;
+  ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE DataWarehouse;
 END;
 GO
 
 -- Create your new database:
-CREATE DATABASE DataWarehouse_Baraa;
+CREATE DATABASE DataWarehouse;
 
 -- Switch to  your database:
-USE DataWarehouse_Baraa;
+USE DataWarehouse;
 
 -- Create your new schema:
 CREATE SCHEMA bronze;
